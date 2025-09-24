23749df69ec1582aeadbc36461cde78ece905f4c
Marcelo Nobrega <nobregamf29@hotmail.com>
2025-09-24 04:17:10 -0300

    Corrigindo metodos HTTP e adicionando o MongoDB

--- server.js
+++ server.js
@@ -1,9 +1,20 @@
 import express from 'express'
 
 const app = express()
+app.use(express.json())
+
+const usersList = []
+
+app.post('/users', (req, res) => {
+
+    usersList.push(req.body)
+
+    res.status(201).json(req.body)
+})
+
 
 app.get('/users', (req, res) => {
-    res.send('ok')
+    res.status(200).json(usersList)
 })
 
 app.listen(3000)
